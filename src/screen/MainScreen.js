008edfcb91a3ac76d86b5dc8188123f529948b59
congduc2304 <congduc2304@gmail.com>
2020-09-13 09:55:37 +0700

    box

---
+++ src/screen/MainScreen.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+import Box from '../components/Box'
+
+
+export  default class MainScreen extends Component {
+    render() {
+        return <Box/>
+        
+    }
+}
